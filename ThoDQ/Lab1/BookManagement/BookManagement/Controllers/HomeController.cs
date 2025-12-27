761cc72143a6e2521e006b27178ac7cfdabcfca7
thodq <doquangtho.com>
2025-12-27 10:52:10 +0700

    Add Lab1 - BookManagement

---
+++ ThoDQ/Lab1/BookManagement/BookManagement/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy() => View();
+        public IActionResult Help() => View();
+    }
+}
---
+++ ThoDQ/Lab1/BookManagement/BookManagement/Data/BookRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookManagement.Models;
+
+namespace BookManagement.Data
+{
+    public static class BookRepository
+    {
+        private static List<Book> books = new()
+        {
+            new Book{ Id = 1, Title = "Doremon", Author = "Kenzo" },
+            new Book{ Id = 2, Title = "Cuốn theo chiều gió", Author = "Rivado"}
+        };
+        
+        private static int nextId = 3;
+
+        public static List<Book> GetAll() => books;
+
+        public static Book? GetById(int id) => books.FirstOrDefault(b => b.Id == id);
+
+        public static void Add(Book book)
+        {
+            book.Id = nextId++;
+            books.Add(book);
+        }
+
+        public static bool Update(Book book)
+        {
+            var index = books.FindIndex(b => b.Id == book.Id);
+            if (index == -1) return false;
+            books[index] = book;
+            return true;
+        }
+    }
+}
